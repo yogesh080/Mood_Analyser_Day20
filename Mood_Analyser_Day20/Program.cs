d1d454cbfa291bca4c8a0ff4addc7be7e1854359
Yogesh <yogeshbietjhs@gmail.com>
2022-07-29 11:30:25 +0530

    Mood response sad and happy

---
+++ Mood_Analyser_Day20/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MoodAnalyser_Day20
+
+{
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ TestMoodAnalyser/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestMoodAnalyser
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
