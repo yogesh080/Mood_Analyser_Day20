db3e5a7fedf3935557487ecc9b166ca87b75ffb7
Yogesh <yogeshbietjhs@gmail.com>
2022-07-29 11:57:39 +0530

    Modd analyser Factory added

---
+++ Mood_Analyser_Day20/MoodAnalyzerFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser_Day20
+{
+    public class MoodAnalyzerFactory
+    {
+        public static object CreateMoodAnalysis(string className, string constructorName)
+        {
+            string pattern = @"."+ constructorName +"$";
+            Match result = Regex.Match(className, pattern);
+
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalysisCustomException(MoodAnalysisCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalysisCustomException(MoodAnalysisCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+            }
+        }
+
+    }
+}
--- TestMoodAnalyser/UnitTest1.cs
+++ TestMoodAnalyser/UnitTest1.cs
@@ -52,7 +52,7 @@ namespace TestMoodAnalyser
 
             //Arrange
             string message = null;
-            string expected_Output = "Enterd Null Input";
+            string expected_Output = "Mood should not be null";
             string actual_Output;
 
             MoodAnalyser moodAnaylyser = new MoodAnalyser(message);
@@ -65,5 +65,93 @@ namespace TestMoodAnalyser
             Assert.AreEqual(expected_Output, actual_Output);
 
         }
+
+
+        [TestMethod]
+        public void AnalyseMood_EmptyTest()
+        {
+
+            //Arrange
+            string message = "";
+            string expected_Output = "Mood should not be Empty";
+            string actual_Output;
+
+            MoodAnalyser moodAnaylyser = new MoodAnalyser(message);
+
+            //Act
+
+            actual_Output = moodAnaylyser.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected_Output, actual_Output);
+
+        }
+
+        /// <summary>
+        /// Test case 4.1 Given MoodAnalyse class Name should return Moodanalyser object.
+        /// </summary>
+
+        [TestMethod]
+        public void ClassNameReturnSameObject()
+        {
+            //Arrange
+            string message = null;
+            object expected = new MoodAnalyser(message);
+            object actual;
+
+            //Act
+            actual = MoodAnalyzerFactory.CreateMoodAnalysis("MoodAnalyser_Day20.MoodAnalyser", "MoodAnalyser");
+
+            //Assert
+            expected.Equals(actual);
+        }
+
+
+
+        /// <summary>
+        /// Test Case 4.2 Given improper class name should throw MoodAnalysisException.
+        /// </summary>
+        [TestMethod]
+        public void GivenImproperClassNameShouldThrowMoodAnalyserCustomeException()
+        {
+            //Arrange
+            string expected = "Class Not Found";
+
+
+            try
+            {
+                //Act
+                object moodAnalyseObject = MoodAnalyzerFactory.CreateMoodAnalysis("MoodAnalyser.DemoClass", "DemoClass");
+            }
+            catch (MoodAnalysisCustomException e)
+            {
+                //Assert
+                Assert.AreEqual(expected, e.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Test Case 4.3 Given Improper Constructor name should throw MoodAnalysisCustomException.
+        /// </summary>
+        [TestMethod]
+        public void GivenImproperConstructorNameShouldThrowMoodAnalyserCustomeException()
+        {
+            //Arrange
+            string expected = "Constructor is Not Found";
+            try
+            {
+                //Act
+                object moodAnalyseObject = MoodAnalyzerFactory.CreateMoodAnalysis("DemoClass", "MoodAnalyser");
+            }
+            catch (MoodAnalysisCustomException e)
+            {
+                //Assert
+                Assert.AreEqual(expected, e.Message);
+            }
+        }
+
+
+
     }
 }
