84ecb74e815f95e309fb861ce3a6d53eeaca11eb
Yogesh <yogeshbietjhs@gmail.com>
2022-07-29 12:04:20 +0530

    Custom Exception added

--- Mood_Analyser_Day20/MoodAnalysisCustomException.cs
+++ Mood_Analyser_Day20/MoodAnalysisCustomException.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mood_Analyser_Day20
+namespace MoodAnalyser_Day20
 {
-    internal class MoodAnalysisCustomException
+    public class MoodAnalysisCustomException : Exception
     {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE, EMPTY_MESSAGE,
+            NO_SUCH_FIELD, NO_SUCH_METHOD,
+            NO_SUCH_CLASS, OBJECT_CREATION_ISSUE
+        }
+
+        private readonly ExceptionType type;
+
+
+        public MoodAnalysisCustomException(ExceptionType type, string message): base(message)
+        {
+            this.type = type;
+        }
     }
 }
